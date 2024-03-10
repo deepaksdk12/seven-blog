ec846b60705174504c8ab5f3a0b003bf6930d90e
deepaksdk12 <156043792+deepaksdk12@users.noreply.github.com>
2024-03-10 12:01:49 +0530

    Update Posts.jsx

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,6 +3,10 @@ import "./posts.css";
 import Post from "../post/Post";
 
 const Posts = ({ posts }) => {
+
+   if (!Array.isArray(posts)) {
+    return <div>No posts available</div>;
+  }
   return (
     <div className="posts">
       {posts.map((p) => (
