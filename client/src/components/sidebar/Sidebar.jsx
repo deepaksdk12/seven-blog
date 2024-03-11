f64215549cd4477bc2291e60a1a14b081c8049f8
deepakkumar <deepakkumar.sm@zohocorp.com>
2024-03-11 20:36:57 +0530

    proxy removed

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -9,7 +9,9 @@ const Sidebar = () => {
 
   useEffect(() => {
     const getCats = async () => {
-      const res = await axios.get("/categories");
+      const res = await axios.get(
+        "https://seven-blog.onrender.com/api/categories"
+      );
       setCats(res.data);
     };
     getCats();
